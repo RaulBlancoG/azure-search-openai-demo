62f459fca9c084c22930446ed1999c53ed342e81
VictorSaldivarGB <118920872+VictorSaldivarGB@users.noreply.github.com>
2023-09-14 17:42:39 +0000

    booton feedback

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -1,51 +1,105 @@
+import React, { useState } from "react";
 import { Outlet, NavLink, Link } from "react-router-dom";
-
 import dna_logo from "../../assets/powered_dna_blanco.png";
-
 import styles from "./Layout.module.css";
+import Swal from 'sweetalert2';
 
 const Layout = () => {
-    return (
-        <div className={styles.layout}>
-            <header className={styles.header} role={"banner"}>
-                <div className={styles.headerContainer}>
-                    <Link to="/" className={styles.headerTitleContainer}>
-                        <h3 className={styles.headerTitle}>Grupo Bimbo ChatGPT | Demo</h3>
-                    </Link>
-                    <nav>
-                        <ul className={styles.headerNavList}>
-                            <li>
-                                <NavLink to="/" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
-                                    Chat
-                                </NavLink>
-                            </li>
-                            <li className={styles.headerNavLeftMargin}>
-                                <NavLink to="/qa" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
-                                    Ask a question
-                                </NavLink>
-                            </li>        
-                        </ul>
-                    </nav>
-                    <li className={styles.headerNavLeftMargin}>
-                                <a href="https://apps.powerapps.com/play/e/9747ef38-2157-4070-a717-06c06a45c8e3/a/5da93623-b160-450e-aea9-57b217b56763?tenantId=973ba820-4a58-4246-84bf-170e50b3152a&source=AppSharedV3&hint=326f04c7-3009-48e7-bae4-bfb3c5e24684
-" 
-title="Link to DnA AI Apps">
-                                    <img
-                                        src={dna_logo}
-                                        alt="DnAlogo"
-                                        aria-label="Link to DnA AI Apps"
-                                        height="400px"
-                                        className={styles.githubLogo}
-                                    />
-                                </a>
-                            </li>
-                    <h4 className={styles.headerRightText}>DEV Version</h4>
-                </div>
-            </header>
+  const [selectedFeedback, setSelectedFeedback] = useState("Feedback"); // Inicialmente, se selecciona "Feedback"
 
-            <Outlet />
+  const feedbackOptions = ["Report a problem", "Make a suggestion", "Share an idea"];
+
+  const handleFeedbackChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedValue = event.target.value;
+    console.log("Opción seleccionada:", selectedValue);
+  
+    // Abre un modal de Swal cuando se selecciona "Make a suggestion"
+    if (selectedValue === "make_a_suggestion"  || selectedValue === "share_an_idea" || selectedValue === "report_a_problem") {
+      Swal.fire({
+        title: 'Make a suggestion',
+        html: `
+          <iframe width="640px" height="480px" src="https://forms.office.com/Pages/ResponsePage.aspx?id=IKg7l1hKRkKEvxcOULMVKgaikAIJ2B9IlnsanlMdSlpUNkNZNkIxNzVXSThLR0pQUUg2RE9ZOVNJQS4u&embed=true" frameborder="0" marginwidth="0" marginheight="0" style="border: none; max-width:100%; max-height:100vh" allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
+        `,
+        preConfirm: function () {
+          // Aquí puedes obtener los valores de los campos de entrada
+          // const problemDescription = document.getElementById('problem-description').value;
+        }
+      });
+    }
+    // Establecer "Feedback" como la opción seleccionada nuevamente
+    setSelectedFeedback("Feedback");
+  };
+
+  return (
+    <div className={styles.layout}>
+      <header className={styles.header} role={"banner"}>
+        <div className={styles.headerContainer}>
+          <Link to="/" className={styles.headerTitleContainer}>
+            <h3 className={styles.headerTitle}>Grupo Bimbo ChatGPT | Demo</h3>
+          </Link>
+          <nav>
+            <ul className={styles.headerNavList}>
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink
+                  }
+                >
+                  Chat
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink
+                  }
+                >
+                  Chat PRUEBA
+                </NavLink>
+              </li>
+              <li className={styles.headerNavLeftMargin}>
+                <NavLink
+                  to="/qa"
+                  className={({ isActive }) =>
+                    isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink
+                  }
+                >
+                  Ask a question
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+          {/* Botón de Feedback */}
+          <div className={styles.feedbackContainer}>
+            <select
+            value={selectedFeedback}
+            onChange={handleFeedbackChange}
+            onInput={handleFeedbackChange}
+            className={`${styles.feedbackSelect}`}
+            >
+              <option value="Feedback" disabled selected>
+                Feedback
+              </option>
+              {feedbackOptions.map((option) => (
+                <option
+                  key={option}
+                  className={`${styles.feedbackSelectOption}`}
+                  value={option.toLowerCase().replace(/\s/g, "_")} // Convierte en minúsculas y reemplaza espacios
+                >
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+          <h4 className={styles.headerRightText}>DEV Version</h4>
         </div>
-    );
+      </header>
+
+      <Outlet />
+    </div>
+  );
 };
 
 export default Layout;

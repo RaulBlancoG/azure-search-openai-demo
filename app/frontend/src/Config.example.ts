a74e14a2f7d22b269df8d8c4382e7ff7d16d1a6e
VictorSaldivarGB <118920872+VictorSaldivarGB@users.noreply.github.com>
2023-11-30 13:28:42 +0000

    Correct URL domain

--- app/frontend/src/Config.example.ts
+++ app/frontend/src/Config.example.ts
@@ -4,8 +4,9 @@
 const config = {
     appId: "84577f1a-cfbb-430f-8b19-627f579c439d",
     authority: "https://login.microsoftonline.com/973ba820-4a58-4246-84bf-170e50b3152a",
-    redirectUri: "http://localhost:5000/",
+    redirectUri: "https://policyai.bimboconnect.com/",
     // https://policyai.bimboconnect.com/
+    // http://localhost:5000/ Test URL
     scopes: ["user.read", "mailboxsettings.read", "calendars.readwrite"]
 };
 // Tenant 973ba820-4a58-4246-84bf-170e50b3152a

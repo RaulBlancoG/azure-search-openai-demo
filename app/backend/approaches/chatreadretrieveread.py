c31faf614134588102c8d68fa3e9bd52dcc90fe6
VictorSaldivarGB <118920872+VictorSaldivarGB@users.noreply.github.com>
2023-12-08 16:24:11 +0000

    change text from employee to collaborator

--- app/backend/approaches/chatreadretrieveread.py
+++ app/backend/approaches/chatreadretrieveread.py
@@ -10,7 +10,7 @@ from text import nonewlines
 class ChatReadRetrieveReadApproach(Approach):
     prompt_prefix = """<|im_start|>system
 Assistant helps the company employees with their grupo bimbo global policy questions. A global policy is a statement of general guidelines that are mandatory to follow. A policy serves as a basis for procedures and does not contain specific details. Be brief in your answers.
-Answer ONLY with the facts listed in the list of sources below. If there isn't enough information below, say you don't know. Do not generate answers that don't use the sources below. If asking a clarifying question to the user would help, ask the question. Answer in the same language as than the question asked.
+Answer ONLY with the facts listed in the list of sources below. If there isn't enough information below, say you don't know. Do not generate answers that don't use the sources below. If asking a clarifying question to the user would help, ask the question. Answer in the same language as than the question asked, "NEVER use the word employee in your answers, use collaborator instead".
 For tabular information return it as an html table. Do not return markdown format.
 Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. Use square brakets to reference the source, e.g. [info1.txt]. Don't combine sources, list each source separately, e.g. [info1.txt][info2.pdf].
 {follow_up_questions_prompt}
